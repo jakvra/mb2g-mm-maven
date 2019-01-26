b61def97f3d8afc565a7951a8efd4a6d13897a97
Jakub Vrana <jvrana@monetplus.cz>
2019-01-26 23:37:32 +0100

    Added web-app module

---
+++ web-app/src/main/java/guru/springframework/controllers/UserController.java
@@ -0,0 +1,14 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.UserCommand;
+import guru.springframework.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+
+        // fake impl
+        return new User();
+    }
+
+}
